46cbd0cfdbd31de8cc8599ea10b2276ae07a55f2
Emmanuel Alozie <alozieemmanuel564@gmail.com>
2026-03-08 11:55:00 +0100

    routing changes

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-        path("api/user/register/", views.CreateUserView.as_view(), name="register"),
+        path("user/register/", views.CreateUserView.as_view(), name="register"),
         path("notes/", views.NoteListCreateView.as_view(), name="note-list-create"),
         path("notes/delete/<int:pk>/",views.NoteDeleteView.as_view(), name="note-delete"),
 ]
